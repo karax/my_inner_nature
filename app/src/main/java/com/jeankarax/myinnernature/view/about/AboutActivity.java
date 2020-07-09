d55ba42e896519963d5e7f3485a460da6897efa0
Jean Karax <jpkarax@scopus.com.br>
2020-07-09 17:17:25 -0300

    First commit

---
+++ app/src/main/java/com/jeankarax/myinnernature/view/about/AboutActivity.java
@@ -0,0 +1,21 @@
+package com.jeankarax.myinnernature.view.about;
+
+import android.os.Bundle;
+
+import com.jeankarax.myinnernature.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Jean Karax on 09/07/2020.
+ */
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
