b3818c7be909ffaaef90a26a221837db2472a1ff
Jean Karax <jpkarax@scopus.com.br>
2020-07-13 17:33:01 -0300

    Added icons animations and ViewPager

--- app/src/main/java/com/jeankarax/myinnernature/view/mainmenu/MainMenuActivity.java
+++ app/src/main/java/com/jeankarax/myinnernature/view/mainmenu/MainMenuActivity.java
@@ -1,6 +1,11 @@
 package com.jeankarax.myinnernature.view.mainmenu;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
 import com.jeankarax.myinnernature.R;
@@ -9,11 +14,12 @@ import com.jeankarax.myinnernature.databinding.ActivityMainMenuBinding;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentActivity;
 
 /**
  * Created by Jean Karax on 13/07/2020.
  */
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends FragmentActivity {
 
     private ActivityMainMenuBinding binding;
 
@@ -23,14 +29,54 @@ public class MainMenuActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main_menu);
         binding.llMyFermentations.setOnClickListener( listener -> {
-            Toast.makeText(this, "Fermentações", Toast.LENGTH_SHORT).show();
+            animateIcons();
         });
         binding.llMyPlants.setOnClickListener(listener -> {
-            Toast.makeText(this, "Plantas", Toast.LENGTH_SHORT).show();
+            animateIcons();
         });
         binding.ibAddButton.setOnClickListener(listener -> {
             Toast.makeText(this, "Novo item", Toast.LENGTH_SHORT).show();
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        resetIcons();
+    }
+
+    private void resetIcons() {
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animatorFermentationX = ObjectAnimator.ofFloat(binding.llMyFermentations, "translationX", 0f);
+        ObjectAnimator animatorFermentationY = ObjectAnimator.ofFloat(binding.llMyFermentations, "translationY", 0f);
+        ObjectAnimator animatorPlantX = ObjectAnimator.ofFloat(binding.llMyPlants, "translationX", 0f);
+        ObjectAnimator animatorPlantY = ObjectAnimator.ofFloat(binding.llMyPlants, "translationY", 0f);
+        animatorSet.playTogether(animatorFermentationX, animatorFermentationY, animatorPlantX, animatorPlantY);
+        animatorSet.setInterpolator(new LinearInterpolator());
+        animatorSet.setDuration(100);
+        animatorSet.start();
+        binding.llMyPlants.setVisibility(View.VISIBLE);
+        binding.llMyFermentations.setVisibility(View.VISIBLE);
+        binding.ivFermentationTab.setVisibility(View.GONE);
+        binding.ivPlantTab.setVisibility(View.GONE);
+    }
+
+    private void animateIcons() {
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animatorFermentationX = ObjectAnimator.ofFloat(binding.llMyFermentations, "translationX", 300f);
+        ObjectAnimator animatorFermentationY = ObjectAnimator.ofFloat(binding.llMyFermentations, "translationY", 600f);
+        ObjectAnimator animatorPlantX = ObjectAnimator.ofFloat(binding.llMyPlants, "translationX", -300f);
+        ObjectAnimator animatorPlantY = ObjectAnimator.ofFloat(binding.llMyPlants, "translationY", 1000f);
+        animatorSet.playTogether(animatorFermentationX, animatorFermentationY, animatorPlantX, animatorPlantY);
+        animatorSet.setInterpolator(new LinearInterpolator());
+        animatorSet.setDuration(100);
+        animatorSet.start();
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            binding.llMyPlants.setVisibility(View.GONE);
+            binding.llMyFermentations.setVisibility(View.GONE);
+            binding.ivFermentationTab.setVisibility(View.VISIBLE);
+            binding.ivPlantTab.setVisibility(View.VISIBLE);
+            }, 100);
     }
 }
